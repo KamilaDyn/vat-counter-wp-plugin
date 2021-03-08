ea2d5dd39a184a772c0ca05d156d05aa23663e32
Kamila <kamila.dynysiuk@gmail.com>
2021-03-08 16:32:32 +0000

    add prefix

--- vat-counter/uninstall.php
+++ vat-counter/uninstall.php
@@ -11,6 +11,8 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 // clean DB via sql
 
 global $wpdb;
+$tablePostMeta = $wpdb->prefix . 'postmeta';
+$tablePosts = $wpdb->prefix . 'posts';
 
-$wpdb->query("DELETE FROM wp_posts WHERE post_type = 'vat_calculations'");
-$wpdb->query("DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
+$wpdb->query("DELETE FROM $tablePosts WHERE post_type = 'vat_calculations'");
+$wpdb->query("DELETE FROM $tablePostMeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
